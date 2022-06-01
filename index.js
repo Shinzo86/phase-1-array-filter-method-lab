bda1deb36a4d7faef199b05666a68bf9fbd0af6f
Adrian Alberto <adrianalberto1986@gmail.com>
2022-05-31 20:39:53 -0500

    commit tree 4f68f88d32ed976a2f5f1e8f99ea24cbfd06d10f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, match){
+    const filterDrivers = drivers.filter(function (driver){
+        return driver.toLowerCase() === match.toLowerCase()
+    })
+    return filterDrivers
+}
+
+function fuzzyMatch(drivers, match) {
+    const filterDrivers = drivers.filter(function (driver){
+        return driver.startsWith(match)
+    })
+    return filterDrivers
+}
+
+function matchName(drivers, match){
+    const filterDrivers = drivers.filter(function (driver){
+        return driver.name === match
+    })
+    return filterDrivers
+}
